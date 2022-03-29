ddae6afa62e9f8eeda681ce0d6d4d0b90a0edb87
Braga Lund <bragalund@gmail.com>
2022-03-29 20:34:03 +0200

    Added two simple spring boot projects with actuator, sping-web, camel and spring-devtools

---
+++ app-a/src/test/java/com/bragalund/camel/appa/AppAApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bragalund.camel.appa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppAApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
